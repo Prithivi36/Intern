adff8ba32b71e9e28584839c65fa10befe4c5a60
Prithivi36 <prithivipappian@gmail.com>
2024-05-10 12:38:16 +0530

    Exception Handeled

--- Base64/src/main/java/Base64Conversion/Base64/ConversionService/ConversionService.java
+++ Base64/src/main/java/Base64Conversion/Base64/ConversionService/ConversionService.java
@@ -3,6 +3,7 @@ package Base64Conversion.Base64.ConversionService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,10 +24,21 @@ public class ConversionService {
 
     public String convert(String AwbNo){
 
-        ResponseEntity<byte []> responseEntity = restTemplate.getForEntity(baseUrl+AwbNo+".jpg", byte[].class);
+        try {
+            ResponseEntity<byte []> responseEntity = restTemplate.getForEntity(baseUrl+AwbNo+".jpg", byte[].class);
 
-        byte[] imageData = responseEntity.getBody();
-        String base64Image = Base64.getEncoder().encodeToString(imageData);
-        return base64Image;
+            if(responseEntity.getStatusCode().is2xxSuccessful()){
+
+                byte[] imageData = responseEntity.getBody();
+                String base64Image = Base64.getEncoder().encodeToString(imageData);
+                return base64Image;
+            }else{
+                return "Failed to retrive Image , Excited with Statscode : " + responseEntity.getStatusCode();
+            }
+        }catch (Exception e){
+            return "Failed to retrive Image , Excited with Statscode : " + HttpStatus.NOT_FOUND;
+        }
+
+
     }
 }
